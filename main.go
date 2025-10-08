bc52560eca393258228e0603281cd4d51af1f27e
cgil <lao.tseu.is.alive@gmail.com>
2025-10-08 17:40:18 +0200

    first simple version

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "github.com/lao-tseu-is-alive/go-cloud-grpc-client-ai/inference" // Change to your module name
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	// Set up a connection to the server.
+	conn, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	// Create a client
+	c := pb.NewInferencerClient(conn)
+
+	// Contact the server and print out its response.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	prompt := "Hello AI world"
+	log.Printf("Sending prompt to Python service: %s", prompt)
+
+	r, err := c.GenerateText(ctx, &pb.GenerateRequest{Prompt: prompt})
+	if err != nil {
+		log.Fatalf("could not generate text: %v", err)
+	}
+
+	log.Printf("Received from Python: %s", r.GetGeneratedText())
+}
