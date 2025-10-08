768544b6ae5428001e0fa6c31e9b344a193e69f0
cgil <lao.tseu.is.alive@gmail.com>
2025-10-08 17:46:04 +0200

    preparing to tag rel

---
+++ pkg/version/version.go
@@ -0,0 +1,10 @@
+package version
+
+var (
+	APP        = "goCloudGrpcClientAi"
+	AppSnake   = "go-cloud-grpc-client-ai"
+	VERSION    = "0.1.0"
+	REPOSITORY = "https://github.com/lao-tseu-is-alive/go-cloud-grpc-client-ai"
+	REVISION   = "unknown"
+	BuildStamp = "unknown"
+)
